29e731938363d563cbeb2acd992431f891a3b5a3
YoonJinJung <yj2135589@g.skku.edu>
2023-03-30 20:29:06 +0900

    even_list

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from typing import List
 # Skeleton code for even_list
-def even_list(int_list: List[int]) -> List[int]:
-    """
-    Determines if a number is even and return an even list.
-    Args:
-        int_list: A list of integer.
-    Returns:
-        A list of even integers.
-    """
-    # TODO: Implement even_list
-    pass
+def even_list(int_list):
+    i = 0
+    while(i < len(int_list)):
+        if (int_list[i] % 2 == 1):
+            del(int_list[i])
+            continue
+        i = i+1
+    return int_list
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     """
@@ -26,8 +24,9 @@ def main():
     # Example list
     int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     even_int_list = even_list(int_list)
-    output = sum_of_squares_of_even(even_int_list)
-    print(output)
+    print(even_int_list)
+    #output = sum_of_squares_of_even(even_int_list)
+    #print(output)
 # Boilerplate code
 if __name__ == "__main__":
     main()
