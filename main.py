be4210546684e5111f569f6689eb511d928905f6
YoonJinJung <yj2135589@g.skku.edu>
2023-03-30 20:35:09 +0900

    main.py

--- main.py
+++ main.py
@@ -24,9 +24,8 @@ def main():
     # Example list
     int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     even_int_list = even_list(int_list)
-    print(even_int_list)
-    #output = sum_of_squares_of_even(even_int_list)
-    #print(output)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
 # Boilerplate code
 if __name__ == "__main__":
     main()
